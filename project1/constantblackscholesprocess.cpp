7ca0e21210e0782c2991d2b3fed433e8d36b81b2
Amine ROUATBI <a17rouat@imt-atlantique.fr>
2020-04-05 18:38:58 +0200

    New commit

--- project1/constantblackscholesprocess.cpp
+++ project1/constantblackscholesprocess.cpp
@@ -1,3 +1,32 @@
 
 #include "constantblackscholesprocess.hpp"
 
+namespace QuantLib {
+
+	ConstantBlackScholesProcess::ConstantBlackScholesProcess(const Real& S,
+                   const Rate& r,
+                   const Volatility& sigma,
+		   const Rate& q) :
+		...
+	}
+
+    Real ConstantBlackScholesProcess::x0() const {
+        return S;
+    }
+
+    Real ConstantBlackScholesProcess::drift(Time t, Real x) const {
+	// Returns µ(t,x) which is r(t) - q(t) - 0.5 sigma(t,x)^2
+        return r - q - 0.5*sigma*sigma
+    }
+
+    Real ConstantBlackScholesProcess::diffusion(Time t, Real x) const {
+	// Returns sigma(t,x), constant here?
+        return sigma;
+    }
+
+    Real ConstantBlackScholesProcess::apply(Real x0, Real dx) const {
+	// Drifts x by dx, meant for log(x)
+        return x0 * std::exp(dx);
+    }
+
+}
--- project1/constantblackscholesprocess.hpp
+++ project1/constantblackscholesprocess.hpp
@@ -4,60 +4,26 @@
 namespace QuantLib {
 
     class ConstantBlackScholesProcess : public StochasticProcess1D {
-    //black scholes process
-    public:
-        
-        Real call_option_black_scholes(const Real& S,
-                   const Real& K,
+      public:
+	/* Equation :  d\ln S(t) = (r(t) - q(t) - \frac{\sigma(t, S)^2}{2}) dt + \sigma dW_t. */
+	
+	// Constructor
+        ConstantBlackScholesProcess(const Real& S,
                    const Rate& r,
                    const Volatility& sigma,
-                   const Time& t)
-        {
-            Real time = t;//Actual360().yearFraction(settlement,maturity);
-             Real d1 = (log(S/K)+r*time)/(sigma*sqrt(time))+0.5*sigma*sqrt(time);
-             Real d2 = d1-sigma*sqrt(time);
-             return S*N(d1)-K*exp(-r*time)*N(d2);
-        };
-        
-        Real put_option_black_scholes(const Real& S,
-                   const Real& K,
-                   const Rate& r,
-                   const Volatility& sigma,
-                   const Time& t)
-        {
-            Real time = t;//Actual360().yearFraction(settlement,maturity);
-             Real d1 = (log(S/K)+r*time)/(sigma*sqrt(time))+0.5*sigma*sqrt(time);
-             Real d2 = d1-sigma*sqrt(time);
-             return -S*N(-d1) + K*exp(-r*time)*N(-d2);
-        };
+		   const Rate& q);
 
+	// Interface with StochasticProcess1D
+        Real x0() const;
+        Real drift(Time t, Real x) const;
+        Real diffusion(Time t, Real x) const;
+        Real apply(Real x0, Real dx) const;
 
-        Real N(const Real& z) //cdf of the normal distribution
-        {
-            if (z >  6.0) { return 1.0; };
-            if (z < -6.0) { return 0.0; };
-            Real b1 =  0.31938153;
-            Real b2 = -0.356563782;
-            Real b3 =  1.781477937;
-            Real b4 = -1.821255978;
-            Real b5 =  1.330274429;
-            Real p  =  0.2316419;
-            Real c2 =  0.3989423;
-            Real a=fabs(z);
-            Real t = 1.0/(1.0+a*p);
-            Real b = c2*exp((-z)*(z/2.0));
-            Real n = ((((b5*t+b4)*t+b3)*t+b2)*t+b1)*t;
-            n = 1.0-b*n;
-            if ( z < 0.0 ) n = 1.0 - n;
-            return n;
-        };
-           
-    private:
-        Real K;
+      private:
         Real S ;
         Rate r;
         Volatility sigma ;
-        Time t;
+        Real q;
     };
     
 }
